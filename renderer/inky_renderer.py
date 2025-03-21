cd8d1ba58635005e73b152731e99dfaddc79f0c1
Jay <teario@gmail.com>
2025-03-21 16:18:09 +0000

    Inky renderer now has a minimum time to wait between renders

--- renderer/inky_renderer.py
+++ renderer/inky_renderer.py
@@ -1,14 +1,41 @@
 from inky.auto import auto
+import datetime
+import threading
 
 class InkyRenderer:
-    def __init__(self):
+    def __init__(self, min_render_interval = 60):
         self.inky = auto(ask_user=True, verbose=False)
 
         self.height = self.inky.height
         self.width = self.inky.width
+
+        self.min_render_interval = min_render_interval
+        self.last_render = datetime.datetime.fromtimestamp(0)
+        self.timer = None
+        self.pending_render = None
         return
 
     def render(self, img):
+        if img == None:
+            img = self.pending_render
+            if img == None:
+                return
+            
+        self.pending_render = img
+        now = datetime.datetime.now()
+        time_diff = (now - self.last_render).total_seconds()
+
+        if self.timer and time_diff < self.min_render_interval:
+            return
+
+        if time_diff < self.min_render_interval:
+            self.timer = threading.Timer(self.min_render_interval - time_diff, self.render, [None])
+            self.timer.start()
+            return
+
+        self.timer = None
+        self.last_render = now
+        self.pending_render = None
         img = img.rotate(90,expand=1)
 
         self.inky.set_image(img)
