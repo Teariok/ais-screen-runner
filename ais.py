5b3b648825f81a8dc2e71ef6e7384e4a3438b8d7
Jay <teario@gmail.com>
2025-03-23 15:52:51 +0000

    Added map screen

--- ais.py
+++ ais.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import json
 from dotenv import dotenv_values
+from input.inky_input import InkyInput
 from input_processor import InputProcessor
 from input.keyboard_input import KeyboardInput
 from message_processor import MessageProcessor
+from screen.ship_map_screen import ShipMapScreen
 from ship_tracker import ShipTracker
 from screen_manager import ScreenManager
 from renderer.image_renderer import ImageRenderer
@@ -16,29 +19,29 @@ from queue import Queue
 from threading import Thread
 
 def begin_message_processing():
-    message_processor = MessageProcessor(config["MQTT_ADDR"], int(config["MQTT_PORT"]), config["MQTT_AIS_TOPIC"], ais_message_queue)
+    message_processor = MessageProcessor(env["MQTT_ADDR"], int(env["MQTT_PORT"]), env["MQTT_AIS_TOPIC"], ais_message_queue)
     message_processor.begin_processing()
 
 def begin_ship_tracking():
     # Init the tracker to keep a record of vessels we've seen
-    ship_tracker = ShipTracker(int(config["MAX_DYN_SIZE"]), config["DB_NAME"], ais_message_queue, vessel_update_queue)
+    ship_tracker = ShipTracker(int(env["MAX_DYN_SIZE"]), env["DB_NAME"], ais_message_queue, vessel_update_queue)
 
     # Set the notification zones up from the env
-    zones = config.get("ZONES","").strip('()').split('),(')
+    zones = prefs.get("ZONES",[])
     for zone in zones:
-        parts = zone.split(',')
-        ship_tracker.add_zone((parts[0].strip(), float(parts[1]), float(parts[2]), float(parts[3])))
+        ship_tracker.add_zone((zone["name"], zone["lat"], zone["lon"], zone["radius"]))
 
     ship_tracker.begin_processing()
 
 def begin_screen_updates(no_screen = True):
     renderer = ImageRenderer("output.jpg") if no_screen else InkyRenderer()
     screens = [
-        ShipTableScreen(config["IMG_DIR"], renderer),
-        ShipZoneScreen(config["IMG_DIR"], renderer)
+        ShipZoneScreen(env["IMG_DIR"], renderer),
+        ShipTableScreen(env["IMG_DIR"], renderer),
+        ShipMapScreen(env["IMG_DIR"], renderer, env["MAPBOX_API_KEY"], prefs.get("MAP_BOUNDS", []), prefs.get("MAPBOX_LIGHT_STYLE",""), prefs.get("MAPBOX_DARK_STYLE","")),
     ]
-    screen_manager = ScreenManager(screens, vessel_update_queue)
 
+    screen_manager = ScreenManager(screens, vessel_update_queue)
     screen_manager.begin_processing()
 
 logger = logging.getLogger(__name__)
@@ -48,21 +51,26 @@ if not os.path.exists(".env"):
     logger.critical(".env file does not exist!")
     os._exit(-1)
 
-config = dotenv_values(".env")
+env = dotenv_values(".env")
+prefs = {}
+
+if os.path.exists("user_prefs.json"):
+    with open("user_prefs.json") as prefs_file:
+        prefs = json.load(prefs_file)
 
 ais_message_queue = Queue()
 vessel_update_queue = Queue()
 
-mpt = Thread(target=begin_message_processing)
-mpt.start()
+msg_proc_thread = Thread(target=begin_message_processing)
+msg_proc_thread.start()
 
-stt = Thread(target=begin_ship_tracking)
-stt.start()
+ship_track_thread = Thread(target=begin_ship_tracking)
+ship_track_thread.start()
 
-sut = Thread(target=begin_screen_updates)
-sut.start()
+screen_update_thread = Thread(target=begin_screen_updates, args=[False])
+screen_update_thread.start()
 
-input_processor = InputProcessor(KeyboardInput())
+input_processor = InputProcessor(InkyInput()) #KeyboardInput())
 
 while True:
     key_val = input_processor.get_key()
---
+++ screen/ship_map_screen.py
@@ -0,0 +1,127 @@
+from font_hanken_grotesk import HankenGroteskBold
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+import datetime
+import threading
+import logging
+from screen.screen_base import ScreenBase
+import os
+import urllib
+
+class ShipMapScreen(ScreenBase):
+    def __init__(self, img_dir, renderer, api_key, bounds, light_style, dark_style, time_window = 60 * 5, render_interval = 10, max_tracked = 20):
+        super().__init__(img_dir, renderer)
+        self.logger = logging.getLogger(__name__)
+
+        self.api_key = api_key
+        self.light_style = light_style
+        self.dark_style = dark_style
+
+        self.visible_ships = {}
+
+        self.hanken_bold_20 = ImageFont.truetype(HankenGroteskBold, 20)
+        self.hanken_bold_14 = ImageFont.truetype(HankenGroteskBold, 14)
+        self.hanken_bold_8 = ImageFont.truetype(HankenGroteskBold, 8)
+
+        self.max_tracked = max_tracked
+        self.render_interval = render_interval
+
+        self.time_window = time_window
+
+        self.bounds = bounds
+        self.min_lon = float(bounds[0])
+        self.min_lat = float(bounds[1])
+        self.max_lon = float(bounds[2])
+        self.max_lat = float(bounds[3])
+
+        self.timer = None
+
+        self.__load_maps()
+
+        self._load_icon("ship", "icon_ship.png", self._LARGE_ICON_SIZE)
+
+        self.map_image = Image.open(os.path.join(self.img_dir, "light.png"))
+
+    def __load_maps(self):
+        for map in [("light",self.light_style),("dark",self.dark_style)]:
+            img_path = os.path.join(self.img_dir, map[0]+".png")
+            if os.path.exists(img_path):
+                continue
+
+            try:
+                style = map[1]
+                size = f"{self.renderer.height}x{self.renderer.width}"
+                bounds = f"[{self.bounds[0],self.bounds[1],self.bounds[2],self.bounds[3]}]"
+
+                url = f"https://api.mapbox.com/styles/v1/mapbox/{style}/static/{bounds}/{size}?access_token={self.api_key}"
+                urllib.request.urlretrieve(url, img_path)
+                print(f"Image has been downloaded and saved as '{img_path}'")
+            except Exception as e:
+                print(f"Failed to download the image. Error: {e}")
+
+    def set_active(self, active):
+        super().set_active(active)
+        if self.active:
+            self.__handle_timer()
+        else:
+            self.timer.cancel()
+            self.timer = None
+
+    def set_mode(self, dark_mode):
+        img_name = "dark.png" if dark_mode else "light.png"
+        self.map_image = Image.open(os.path.join(self.img_dir, img_name))
+        super().set_mode(dark_mode)
+
+    def __handle_timer(self):
+        self.timer = threading.Timer(self.render_interval, self.__handle_timer)
+        self.timer.start()
+        self._render_screen()
+
+    def update(self, msg):
+        if msg[0] != "update":
+            return
+        
+        ship = msg[1]
+
+        self.visible_ships[ship['mmsi']] = ship
+        self.visible_ships = dict(sorted(self.visible_ships.items(), key=lambda item: item[1]['ts'], reverse=True)[:self.max_tracked])
+
+    def _render_screen(self, force = False):
+        if not self.active:
+            return
+        
+        self.logger.info(f"Draw Map")
+
+        img = Image.new("RGB", (self.width,self.height), color=self.BLUE)
+        draw = ImageDraw.Draw(img)
+
+        img.paste(self.map_image)
+
+        now = datetime.datetime.now()
+
+        for ship in self.visible_ships.values():
+            ts = datetime.datetime.fromtimestamp(ship.get('ts', 0))
+
+            if (now - ts).total_seconds() > self.time_window:
+                continue
+
+            point_lat = ship.get("lat", 0)
+            point_lon = ship.get("lon", 0)
+
+            if point_lat < self.min_lat or point_lat > self.max_lat:
+                continue
+
+            if point_lon < self.min_lon or point_lon > self.max_lon:
+                continue
+
+            point_y = self.height - (((point_lat - self.min_lat) / (self.max_lat - self.min_lat)) * self.height)
+            point_x = ((point_lon - self.min_lon) / (self.max_lon - self.min_lon)) * self.width
+
+            point_size = 5
+            text_offset_y = 5
+
+            draw.ellipse([point_x-point_size/2,point_y-point_size/2,point_x+point_size/2,point_y+point_size/2], self.YELLOW if self._dark_mode else self.BLACK)
+            text_size = self._get_text_size(self.hanken_bold_8, ship.get("name"))
+            draw.text((point_x - (text_size[0]/2), point_y - text_offset_y - (point_size/2) - text_size[1]), ship.get("name"), self.YELLOW if self._dark_mode else self.RED, font=self.hanken_bold_8)
+
+        img = img.convert("RGB")
+        self.renderer.render(img, force)
