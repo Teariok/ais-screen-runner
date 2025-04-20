e288aa3055ae7d5fa1c820b195d1ecdcd2680891
Jay <teario@gmail.com>
2025-04-20 15:17:16 +0100

    daisy message source read block instead of 1 byte at a time

--- message/daisy_message_source.py
+++ message/daisy_message_source.py
@@ -12,6 +12,10 @@ class DaisyMessageSource:
     BYTES_AVAIL_L_ADDR = 0xFE
     MESSAGE_BUFF_ADDR = 0xFF
 
+    MAX_BLOCK_SIZE = 32
+    READ_INTERVAL = 0.05
+    ERROR_WAIT_TIME = 0.01
+
     def __init__(self):
         self.logger:logging.Logger = logging.getLogger(__name__)
         self.bus = SMBus(1)
@@ -20,14 +24,31 @@ class DaisyMessageSource:
     def read_byte(self, addr):
         self.bus.write_byte(self.DEVICE_ADDR, addr)
         return self.bus.read_byte(self.DEVICE_ADDR)
+    
+    def read_block(self, size: int) -> str:
+        buff = bytearray()
+        while size > 0:
+            buff_size = min(size, self.MAX_BLOCK_SIZE)
+            block = self.bus.read_i2c_block_data(self.DEVICE_ADDR, self.MESSAGE_BUFF_ADDR, buff_size)
+            buff.extend(block)
+            size -= buff_size
+        return bytes(buff)
+
+    def log_device_info(self):
+        device_type = self.read_byte(self.DEVICE_TYPE_ADDR)
+        version_major = self.read_byte(self.FIRMWARE_MAJOR_ADDR)
+        version_minor = self.read_byte(self.FIRMWARE_MINOR_ADDR)
+        self.logger.info(f"dAISy device ID: 0x{device_type:02X}, firmware v{version_major}.{version_minor}")
 
     def begin_processing(self):
+        self.log_device_info()
+
         if self.handle_message is None:
             self.logger.critical("Cannot begin message processing with no message handler")
             return
 
         self.bus.write_byte(self.DEVICE_ADDR, self.MESSAGE_BUFF_ADDR)
-        message = ""
+        message = b""
 
         while True:
             try:
@@ -36,17 +57,18 @@ class DaisyMessageSource:
                 available = (high << 8) | low
 
                 if available == 0:
-                    time.sleep(0.05)
+                    time.sleep(self.READ_INTERVAL)
                     continue
 
-                data = self.bus.read_block_data(self.DEVICE_ADDR, self.MESSAGE_BUFF_ADDR, available)
-                message += bytes(data).decode('utf-8')
+                self.logger.debug(f"Bytes available: {available}")
+                message += self.read_block(available)
 
-                while "\r\n" in message:
-                    complete, _, remainder = message.partition("\r\n")
-                    self.handle_message(complete.strip())
+                while b"\r\n" in message:
+                    complete, _, remainder = message.partition(b"\r\n")
+                    self.logger.info(f"MSG: {complete}")
+                    self.handle_message(complete)
                     message = remainder
 
             except OSError as ex:
                 self.logger.warning(f"Error reading from I2C: {ex}")
-                time.sleep(0.1)
+                time.sleep(self.ERROR_WAIT_TIME)
